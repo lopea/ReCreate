ef9f7885865cb2db50c696819a6b242754bc7338
Javier Sandoval <Javiers2001@live.com>
2020-07-21 22:28:31 -0500

    Added Inital + Hex

---
+++ Assets/Lopea/Creations/HexView/GameObjectPool.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lopea.Tools
+{
+    public class GameObjectPool
+    {
+        GameObject _prefab;
+        int _max;
+
+        /// <summary>
+        /// stores all inactive gameobjects
+        /// </summary>
+        Queue<GameObject> inactiveGO;
+
+        /// <summary>
+        /// stores all active gameObjects
+        /// </summary>
+        List<GameObject> currentGO;
+
+        /// <summary>
+        /// GameObject reference to create more instances of.
+        /// </summary>
+        public GameObject prefab => _prefab;
+
+        /// <summary>
+        /// The maximum instances of the prefab given to spawn
+        ///</summary>
+        public int maxObjects => _max;
+
+        /// <summary>
+        /// Current number of active gameobjects
+        /// </summary>
+        public int ActiveCount => currentGO.Count;
+
+
+        public GameObject this[int index] => currentGO[index];
+        public GameObjectPool(GameObject prefab, int max, Transform parent = null)
+        {
+            if(max >= 0)
+            {
+                throw new System.Exception("Error when creating GameObjectPool: invalid maximum value.");
+            }
+            
+            //initialize lists
+            inactiveGO = new Queue<GameObject>();
+            currentGO = new List<GameObject>();
+
+            //set variables
+            _max = max;
+            _prefab = prefab;
+
+            //create new instances of the prefab
+            for(int i = 0; i < max; i++)
+            {
+                var instance = GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity, parent);
+                instance.SetActive(false);
+
+                //set to the inactive list
+                inactiveGO.Enqueue(instance);
+            }
+        }
+        
+        public GameObject GetNextGameObject()
+        {
+            var next = inactiveGO.Dequeue();
+            if(!next.activeSelf)
+            { 
+                next.SetActive(true);
+                currentGO.Add(next);
+            }
+            inactiveGO.Enqueue(next);
+            return next;
+        }
+
+        public void PopulateActiveList(int count)
+        {
+            for(int i = 0; i < count; i ++)
+            {
+                GetNextGameObject();
+            }
+        }
+        
+        public void Deactivate(GameObject obj)
+        {
+            obj.SetActive(false);
+            currentGO.Remove(obj);
+            inactiveGO.Enqueue(obj);
+        }
+
+    }
+}
---
+++ Assets/Lopea/Creations/HexView/HexManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+namespace Lopea.ReCreate.HexView
+{
+    public class HexManager : MonoBehaviour
+    {
+        List<GameObject> Objects;
+
+        [SerializeField]
+        GameObject prefab;
+
+        [SerializeField]
+        int length = 10;
+
+        [SerializeField]
+        float speed = 10, zCutoff = -5;
+        void Start()
+        {
+            Objects = new List<GameObject>();
+            for (int i = 0; i < length; i++)
+            {
+                var inst = Instantiate(prefab, new Vector3(0, 0, 5 + i * 5), Quaternion.Euler(-90, 0, 180), transform);
+                Objects.Add(inst);
+            }
+        }
+        private float GetNextZValue()
+        {
+            float max = float.MinValue;
+            for (int i = 0; i < Objects.Count; i++)
+            {
+                if (max < Objects[i].transform.position.z)
+                    max = Objects[i].transform.position.z;
+            }
+            return max;
+        }
+        void Update()
+        {
+            for (int i = 0; i < Objects.Count; i++)
+            {
+                Objects[i].transform.Translate(new Vector3(0, 0, -speed) * Time.deltaTime, Space.World);
+
+                if (Objects[i].transform.position.z < zCutoff)
+                {
+                    float max = GetNextZValue() + 5;
+
+                    var pos = Objects[i].transform.position;
+                    pos.z = max;
+                    Objects[i].transform.position = pos;
+                }
+            }
+        }
+    }
+
+}
